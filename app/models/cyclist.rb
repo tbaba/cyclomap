23795e7deb454d6079b9f12c39634a498bad1589
Tatsuro Baba <harakirisoul@gmail.com>
2014-03-25 19:56:19 +0900

    Cyclomap uses 'trackable' and 'omniauthable' devise

--- app/models/cyclist.rb
+++ app/models/cyclist.rb
@@ -1,6 +1,3 @@
 class Cyclist < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :trackable, :omniauthable
 end
