529916c4216733c67abf13fe14880e8a99fd7657
Tatsuro Baba <harakirisoul@gmail.com>
2014-04-03 09:45:27 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402043315) do
+ActiveRecord::Schema.define(version: 20140403004346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20140402043315) do
     t.datetime "updated_at"
   end
 
-  create_table "cyclists", force: true do |t|
+  create_table "users", force: true do |t|
     t.integer  "sign_in_count",                default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
