75563014ef15507f0ba741de74e5df470482b56f
Tatsuro Baba <harakirisoul@gmail.com>
2014-04-04 21:16:49 +0900

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140403041308) do
+ActiveRecord::Schema.define(version: 20140404121615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: true do |t|
-    t.string   "title",      null: false
+    t.string   "title",        null: false
     t.text     "body"
-    t.integer  "user_id",    null: false
+    t.integer  "user_id",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "route_lab_id"
   end
 
   create_table "users", force: true do |t|
