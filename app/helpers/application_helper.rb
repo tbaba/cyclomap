818aed2e6f5bd7041cee1a41169846b668af8c5b
Tatsuro Baba <harakirisoul@gmail.com>
2014-04-05 10:18:41 +0900

    Render route lab iframe player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
+require 'addressable/uri'
+
 module ApplicationHelper
+  def route_lab_course(route_lab_id)
+    url = "http://latlonglab.yahoo.co.jp/route/paste?"
+    url = Addressable::URI.parse url
+    url.query = {
+      id: route_lab_id,
+      width: 640,
+      height: 480,
+      mapstyle: 'map',
+      graph: true,
+      maponly: false
+    }.map{|k, v| "#{k}=#{v}" }.join('&')
+    javascript_include_tag(url.to_s)
+  end
 end
