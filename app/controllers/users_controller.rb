af9f4578c4f2577a11475fc910944b5f98c4dce3
Tatsuro Baba <harakirisoul@gmail.com>
2014-04-10 00:01:48 +0900

    User can edit his information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
     @courses = @user.courses.order('id desc')
     @popular_courses = @user.courses.order('stars_count desc').limit(3)
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      redirect_to @user, notice: 'ユーザー情報を更新しました'
+    else
+      render 'edit'
+    end
+  end
+
   def set_user
     @user = User.find params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:screen_name)
   end
 end
