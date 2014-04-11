5c1f8f1c9e9ebe5850a7e8fe71a84b55560270e8
Tatsuro Baba <harakirisoul@gmail.com>
2014-04-11 09:50:00 +0900

    Add validations for Star

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,4 +1,7 @@
 class Star < ActiveRecord::Base
   belongs_to :user
   belongs_to :course, counter_cache: true
+
+  validates_presence_of :user_id, :course_id
+  validates_uniqueness_of :course_id, scope: :user_id
 end
