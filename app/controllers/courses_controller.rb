c6e82e7ec850b120ff6d6a9dee8b5810a76810db
Tatsuro Baba <harakirisoul@gmail.com>
2014-04-23 20:45:52 +0900

    tagged_courses_path is not require authentication

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show, :tagged]
   before_action :set_course, only: [:show]
   before_action :set_user, only: [:new, :create]
 
